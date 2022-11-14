c506a2a2bfaf5f6aa481a04c687df36f3c5fd657
Utsargo Roy <royutsargo@gmail.com>
2022-11-14 22:26:53 +0600

    Added Eslint configuration

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,11 +2,13 @@ const menuIco = document.querySelector(".menu-ico");
 const navMenu = document.querySelector(".main-menu");
 
 menuIco.addEventListener("click", () => {
-    menuIco.classList.toggle("active");
-    navMenu.classList.toggle("active");
-  })
+  menuIco.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
-  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
     menuIco.classList.remove("active");
     navMenu.classList.remove("active");
-  }))
+  })
+);
