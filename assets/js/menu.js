69a5b5fdbe2057701924fa7038ac4f33d7395aee
Utsargo Roy <royutsargo@gmail.com>
2022-11-14 22:39:50 +0600

    Fixed javascript linter errors

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,14 +1,12 @@
-const menuIco = document.querySelector(".menu-ico");
-const navMenu = document.querySelector(".main-menu");
+const menuIco = document.querySelector('.menu-ico');
+const navMenu = document.querySelector('.main-menu');
 
-menuIco.addEventListener("click", () => {
-  menuIco.classList.toggle("active");
-  navMenu.classList.toggle("active");
+menuIco.addEventListener('click', () => {
+  menuIco.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
 
-document.querySelectorAll(".nav-link").forEach((n) =>
-  n.addEventListener("click", () => {
-    menuIco.classList.remove("active");
-    navMenu.classList.remove("active");
-  })
-);
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  menuIco.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
