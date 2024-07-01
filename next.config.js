83c847b096c1cb71db7f03517f415cfd7787cd2d
Utsargo Roy <royutsargo@gmail.com>
2024-07-01 11:45:03 +0600

    Modify deployment

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-module.exports = {
-    basePath: '/portfolio-roy.github.io',
-    assetPrefix: '/portfolio-roy.github.io/',
-  };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    output: 'export',
+    basePath: '/portfolio-roy.github.io', // Set the base path for your GitHub Pages project
+    assetPrefix: '/portfolio-roy.github.io/', // Set the asset prefix for your GitHub Pages project
+  }
+  
+module.exports = nextConfig
+  
